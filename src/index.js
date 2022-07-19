c95eddf118db4a63145a97f535df3e7465ee72c1
veranikam85 <moruchokv@gmail.com>
2022-07-19 13:31:30 +0300

    feat: edit answer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  open = bracketsConfig.map( number => number[0]);
-  close = bracketsConfig.map( number => number[1]);
-  pair = Object.fromEntries(bracketsConfig);
- let stack=[]
- if (str.length%2!==0){
-  return false;
- }
-   for(let i=0; i<str.length;i++){
-  let bracket=str[i]
-  if(open.includes(bracket)){
-  stack.push(bracket)}
-  else if (close.includes(bracket)){
-  top = stack.pop(); 
-  if(pair[top]!==bracket){ 
-      return false
+  let pair = Object.fromEntries(bracketsConfig);
+  let stack=[]
+  if (str.length%2!==0){
+   return false;
   }
-}
-}
-return stack.length===0}
+   for(let i=0; i<str.length;i++){
+   let top = stack[stack.length-1] 
+   let bracket=str[i]
+   if(pair[top]!==bracket){
+   stack.push(bracket)}
+   else {
+   top=stack.pop();}
+ }
+ return stack.length===0}
